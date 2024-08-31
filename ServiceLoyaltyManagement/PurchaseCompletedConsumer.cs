bdad02d5c86b1099004b3279fcf3b471ffb6ecf1
Kurban Kurbanov <kurban.kurban1998@mail.ru>
2024-08-31 17:35:04 +0300

    add lock

--- ServiceLoyaltyManagement/PurchaseCompletedConsumer.cs
+++ ServiceLoyaltyManagement/PurchaseCompletedConsumer.cs
@@ -22,40 +22,47 @@ namespace ServiceLoyaltyManagement
 
         private void AddBonuses(BasketUser basketUser)
         {
-            var customer = _loyalCustomerRepository.GetByUserName(basketUser.UserName);
-            if(customer != null)
+            lock (_lockObject)
             {
-                if (basketUser.Products.Count() > MinimumQuantityGoodsForBonus)
+                var customer = _loyalCustomerRepository.GetByUserName(basketUser.UserName);
+                if (customer != null)
                 {
-                    customer.Bonuses += Bonus;
-                }
+                    if (basketUser.Products.Count() > MinimumQuantityGoodsForBonus)
+                    {
+                        customer.Bonuses += Bonus;
+                    }
 
-                _loyalCustomerRepository.Update(customer);
+                    _loyalCustomerRepository.Update(customer);
+                }
             }
         }
 
         private void UpsertUser(BasketUser basketUser)
         {
-            var customer = _loyalCustomerRepository.GetByUserName(basketUser.UserName);
-            if(customer != null)
+            lock ( _lockObject)
             {
-                customer.PurchaseNumber += basketUser.Products.Count();
-                _loyalCustomerRepository.Update(customer);
-            }
-            else
-            {
-                _loyalCustomerRepository.Add(new Customer
+                var customer = _loyalCustomerRepository.GetByUserName(basketUser.UserName);
+                if (customer != null)
                 {
-                    Name = basketUser.UserName,
-                    Address = basketUser.Address,
-                    PurchaseNumber = basketUser.Products.Count(),
-                    Bonuses = 0
-                });
+                    customer.PurchaseNumber += basketUser.Products.Count();
+                    _loyalCustomerRepository.Update(customer);
+                }
+                else
+                {
+                    _loyalCustomerRepository.Add(new Customer
+                    {
+                        Name = basketUser.UserName,
+                        Address = basketUser.Address,
+                        PurchaseNumber = basketUser.Products.Count(),
+                        Bonuses = 0
+                    });
+                }
             }
         }
 
         private const int MinimumQuantityGoodsForBonus = 3;
         private const double Bonus = 1000;
+        private readonly object _lockObject = new object();
 
         private readonly ICustomerRepository _loyalCustomerRepository;
     }
